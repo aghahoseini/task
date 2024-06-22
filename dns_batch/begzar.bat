d51842e40a460b38bd5a048521bfc8182eed830d
aghahoseini <aghahoseini.savalan@gmail.com>
2024-06-22 10:35:45 +0330

    Create begzar.bat

---
+++ dns_batch/begzar.bat
@@ -0,0 +1,10 @@
+setlocal EnableDelayedExpansion
+fsutil Dirty Query %SystemDrive% > nul && goto:[RunAs]
+echo CreateObject^("Shell.Application"^). ^
+ShellExecute "%~0","+","","RunAs",1 > "%Temp%\+.vbs" && "%Temp%\+.vbs" & Exit
+:[RunAs]
+
+
+netsh dnsclient delete dnsserver "WI-FI" all
+netsh interface ipv4 add dnsserver "Wi-Fi" address=185.55.226.26 index=1
+netsh interface ipv4 add dnsserver "Wi-Fi" address=185.55.225.25 index=2
